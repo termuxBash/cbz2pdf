a4ecd9cbf343f5ec8b58b473a39909c591fa4677
termuxer <131900623+termuxBash@users.noreply.github.com>
2024-02-02 20:09:20 +0530

    Create bash startup.sh

---
+++ bash startup.sh
@@ -0,0 +1,6 @@
+alias cls="clear"
+st="am start --user 0 -n"
+alias whatsapp="$st com.whatsapp/com.whatsapp.Main"
+alias termux="$st com.termux/com.termux.app.TermuxActivity"
+
+alias cbz="bash /sdcard/cbz2pdf/cbz2pdf.sh"
