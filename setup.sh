daaa68cf64fa181880cff0d9dd0c7844779385bd
termuxer <131900623+termuxBash@users.noreply.github.com>
2024-02-02 20:04:31 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
-echo hi
+echo Setup in progress.
+termux-setup-storage && sleep 4s
+cd /sdcard
+mkdir cbz2pdf
+cd cbz2pdf
+curl -O https://raw.githubusercontent.com/termuxBash/cbz2pdf/main/cbz2pdf.sh
+cd ~/
+curl -o .bashrc https://raw.githubusercontent.com/termuxBash/cbz2pdf/main/bashStartUp.sh
+apt update && apt upgrade
+pkg install -y imagemagick
+pkg install -y pdftk
+pkg install -y megacmd
