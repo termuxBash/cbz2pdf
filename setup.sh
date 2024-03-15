ae02cae791c0f469db4e9418573e7b19e27342b5
termuxer <131900623+termuxBash@users.noreply.github.com>
2024-03-15 20:07:13 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 echo Setup in progress.
 termux-setup-storage && sleep 4s
+source <= 'progress-bar() {   local duration=${1};     already_done() { for ((done=0; done<$elapsed; done++)); do printf "▇"; done };     remaining() { for ((remain=$elapsed; remain<$duration; remain++)); do printf " "; done };     percentage() { printf "| %s%%" $(( (($elapsed)*100)/($duration)*100/100 )); };   for (( elapsed=1; elapsed<=$duration; elapsed++ )); do       already_done; remaining; percentage;       sleep 1;       printf "\r";   done;   printf "\r"; }'
 cd /sdcard
 mkdir cbz2pdf
 cd cbz2pdf
