d1bf44b93641b04778c01a1161ddfad9c4f852b4
termuxer <131900623+termuxBash@users.noreply.github.com>
2024-04-01 21:19:30 +0530

    Update newb.sh

--- newb.sh
+++ newb.sh
@@ -3,7 +3,6 @@ termux-setup-storage && sleep 4s
 source <= 'progress-bar() {   local duration=${1};     already_done() { for ((done=0; done<$elapsed; done++)); do printf "▇"; done };     remaining() { for ((remain=$elapsed; remain<$duration; remain++)); do printf " "; done };     percentage() { printf "| %s%%" $(( (($elapsed)*100)/($duration)*100/100 )); };   for (( elapsed=1; elapsed<=$duration; elapsed++ )); do       already_done; remaining; percentage;       sleep 1;       printf "\r";   done;   printf "\r"; }'
 
 cd ~/
-curl -o .bashrc https://raw.githubusercontent.com/termuxBash/cbz2pdf/main/bashStartUp.sh
 apt update && apt upgrade
 pkg install -y termux-api
 echo Installing the termux API app...
@@ -13,7 +12,9 @@ termux-open termuxBoot.apk
 curl -o termuxApi.apk https://f-droid.org/repo/com.termux.api_51.apk
 termux-open termuxApi.apk
 echo Waiting...
+
 progress-bar 60
+
 pkg install termux-api
 rm termuxBoot.apk termuxApi.apk
 cd ~/.termux
